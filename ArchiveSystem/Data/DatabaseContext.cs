7c8dd41c597fc4a811c77ca157a370453911c2c1
omar abdo <omarfaroq2003@gmail.com>
2026-05-01 22:38:10 +0300

    Add project files.

---
+++ ArchiveSystem/Data/DatabaseContext.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+
+namespace ArchiveSystem.Data
+{
+    public class DatabaseContext(string dbPath)
+    {
+        private readonly string _dbPath = dbPath;
+
+        public SqliteConnection CreateConnection()
+        {
+            var conn = new SqliteConnection($"Data Source={_dbPath}");
+            conn.Open();
+            conn.Execute("PRAGMA foreign_keys = ON;");
+            conn.Execute("PRAGMA journal_mode = WAL;");
+            conn.Execute("PRAGMA busy_timeout = 5000;");
+            return conn;
+        }
+
+        public void InitializeDatabase()
+        {
+            using var conn = CreateConnection();
+            RunMigrations(conn);
+        }
+
+        private void RunMigrations(SqliteConnection conn)
+        {
+            // migrations will be added here in Milestone 2
+            // for now this just ensures the method exists
+        }
+    }
+}
---
+++ ArchiveSystem/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Configuration;
+using System.Windows;
+
+namespace ArchiveSystem
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // open on search page by default
+            MainFrame.Navigate(new SearchPage());
+        }
+
+        private void NavSearch_Click(object sender, RoutedEventArgs e)
+            => MainFrame.Navigate(new SearchPage());
+
+        private void NavEntry_Click(object sender, RoutedEventArgs e)
+            => MainFrame.Navigate(new EntryPage());
+
+        private void NavAllData_Click(object sender, RoutedEventArgs e)
+            => MainFrame.Navigate(new AllDataPage());
+
+        private void NavStats_Click(object sender, RoutedEventArgs e)
+            => MainFrame.Navigate(new StatisticsPage());
+
+        private void NavImport_Click(object sender, RoutedEventArgs e)
+            => MainFrame.Navigate(new ExcelImportPage());
+
+        private void NavReports_Click(object sender, RoutedEventArgs e)
+            => MainFrame.Navigate(new ReportsPage());
+
+        private void NavSettings_Click(object sender, RoutedEventArgs e)
+            => MainFrame.Navigate(new SettingsPage());
+    }
+}
